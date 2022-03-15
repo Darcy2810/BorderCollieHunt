088fec7d82b34d1f6497f01f9e0da0405e281da2
Darcy2810 <0852201@schoolsnet.act.edu.au>
2022-03-15 15:28:24 +1100

    update 1

--- MainGame/MainGame.gd
+++ MainGame/MainGame.gd
@@ -2,12 +2,12 @@ extends Control
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	set_process(true)
+export(int) var countdownmax
+var currentTimer
 
-func _process(delta):
-	pass
-	
+func _ready():
+   currentTimer = countdownmax
+   $HUD/Countdown.text = str (currentTimer)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
